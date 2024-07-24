78b61b54a7726f18948c0b2f1bad89bddae2b997
Vlad <vgozhyi@gmail.com>
2024-07-24 11:25:27 +0000

    Print integer function
    Works more or less, will need to integrate it with the rest of the code

---
+++ printf_digits.h
@@ -0,0 +1,12 @@
+#ifndef PRINT_DIGITS_H
+#define PRINT_DIGITS_H
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+
+unsigned int print_double(va_list arg);
+
+unsigned int print_integer(va_list  arg);
+
+#endif
