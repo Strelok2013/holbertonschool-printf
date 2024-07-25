1594f227fa6398e2eeb12f43065d8b08f1014d8d
phon xay <majic42@hotmail.com>
2024-07-25 08:43:33 +0000

    First commit2

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/types.h>
+
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+#endif
