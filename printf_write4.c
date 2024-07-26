82413e671c4fb43cf0f763fd0b58357d31b9ba34
phon xay <majic42@hotmail.com>
2024-07-26 07:53:02 +0000

    fix logic3

--- printf_write4.c
+++ printf_write4.c
@@ -79,3 +79,44 @@ int _printf(const char *format, ...)
     va_end(args);
     return count;
 }
+
+/**
+ * print_integer - prints an integer
+ *
+ * @args: argument list
+ *
+ * Return: number of characters printed
+ */
+int print_integer(va_list args)
+{
+    int num = va_arg(args, int);
+    char buffer[12];
+    int i = 0, j, temp;
+
+    if (num == 0)
+    {
+        write(1, "0", 1);
+        return 1;
+    }
+
+    if (num < 0)
+    {
+        write(1, "-", 1);
+        num = -num;
+        i++;
+    }
+
+    temp = num;
+    while (temp > 0)
+    {
+        buffer[i++] = (temp % 10) + '0';
+        temp /= 10;
+    }
+
+    for (j = i - 1; j >= 0; j--)
+    {
+        write(1, &buffer[j], 1);
+    }
+
+    return i;
+}
